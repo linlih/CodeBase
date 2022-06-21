d3829138f9b719c7f77facf6cdb5a71746f57464
linlih <linhome.luck@gmail.com>
2022-06-21 23:32:47 +0800

    :construction: 参考Linux内核实现红黑树的插入
    写完了插入，还需要补充删除和应用例子

---
+++ c++/rbtree/rbtree.c
@@ -0,0 +1,105 @@
+//
+// Created by linlh on 22-6-21.
+//
+
+#include "rbtree.h"
+
+static void __rb_rotate_left(struct rb_node *node, struct rb_root *root) {
+    struct rb_node *right = node->rb_right;
+    struct rb_node *parent = rb_parent(node);
+
+    /* 赋值表达式返回的是左边的变量，下面这句话等价于：
+     * node->rb_right = right->rb_left;
+     * if (node->rb_right != NULL)
+     * */
+    if ((node->rb_right = right->rb_left))
+        rb_set_parent(right->rb_left, node);
+    right->rb_left = node;
+
+    rb_set_parent(right, parent);
+
+    if (parent) {
+        if (node == parent->rb_left)
+            parent->rb_left = right;
+        else
+            parent->rb_right = right;
+    } else
+        root->rb_node = right;
+    rb_set_parent(node, right);
+}
+
+static void __rb_rotate_right(struct rb_node *node, struct rb_root *root) {
+    struct rb_node *left = node->rb_left;
+    struct rb_node *parent = rb_parent(node);
+
+    if ((node->rb_left = left->rb_right))
+        rb_set_parent(left->rb_right, node);
+    left->rb_right = node;
+
+    rb_set_parent(left, parent);
+
+    if (parent) {
+        if (node == parent->rb_right)
+            parent->rb_right = left;
+        else
+            parent->rb_left = left;
+    } else
+        root->rb_node = left;
+    rb_set_parent(node, left);
+}
+
+// 根据红黑树的插入逻辑，进行不同的旋转和染色
+void rb_insert_color(struct rb_node *node, struct rb_root *root) {
+    struct rb_node *parent, *gparent;
+
+    while((parent = rb_parent(node) && rb_is_red(parent))) {
+        gparent = rb_parent(parent);
+        if (parent == gparent->rb_left) {
+            {
+                struct rb_node *uncle = gparent->rb_right;
+                // 当uncle节点是红色和parent都是红色的时候
+                if (uncle && rb_is_red(uncle)) {
+                    rb_set_black(uncle);
+                    rb_set_black(parent);
+                    node = gparent;
+                    continue;
+                }
+            }
+            // uncle节点是黑色
+            if (parent->rb_right == node) {
+                register struct rb_node *tmp;
+                __rb_rotate_left(parent, root);
+                tmp = parent;
+                parent = node;
+                node = tmp;
+            }
+            rb_set_black(parent);
+            rb_set_red(gparent);
+            __rb_rotate_right(gparent, root);
+        } else {
+            {
+                struct rb_node *uncle = gparent->rb_left;
+                // 当uncle节点是红色和parent都是红色的时候
+                if (uncle && rb_is_red(uncle)) {
+                    rb_set_black(uncle);
+                    rb_set_black(parent);
+                    node = gparent;
+                    continue;
+                }
+            }
+
+            if (parent->rb_left == node) {
+                register struct rb_node *tmp;
+                __rb_rotate_right(parent, root);
+                tmp = parent;
+                parent = node;
+                node = tmp;
+            }
+
+            rb_set_black(parent);
+            rb_set_red(gparent);
+            __rb_rotate_left(gparent, root);
+        }
+    }
+    rb_set_black(root->rb_node);
+}
---
+++ c++/rbtree/rbtree.h
@@ -0,0 +1,40 @@
+//
+// Created by linlh on 22-6-21.
+//
+
+#ifndef C_RBTREE_H
+#define C_RBTREE_H
+
+struct rb_node {
+    unsigned long rb_parent_color;
+    struct rb_node *rb_right;
+    struct rb_node *rb_left;
+}__attribute__((aligned(sizeof(long))));
+
+struct rb_root {
+    struct rb_node *rb_node;
+};
+
+#define RB_RED   0
+#define RB_BLACK 1
+
+// 利用rb_parent_color存储了父节点的地址
+#define rb_parent(r) ((struct rb_node*)((r)->rb_parent_color) & ~3)
+#define rb_color(r) ((r)->rb_parent_color & 1)
+#define rb_is_red(r) (!rb_color(r))
+#define rb_set_red(r) do { (r)->rb_parent_color &= ~1; } while(0)
+#define rb_set_black(r) do { (r)->rb_parent_color |= 1; } while(0)
+
+static inline void rb_set_parent(struct rb_node *rb, struct rb_node *p) {
+    rb->rb_parent_color = (rb->rb_parent_color & 3) | (unsigned long)p;
+}
+
+static inline void rb_set_color(struct rb_node* rb, int color) {
+    rb->rb_parent_color = (rb->rb_parent_color & ~1) | color;
+}
+
+#define RB_ROOT (struct rb_root) {NULL,}
+
+void rb_insert_color(struct rb_node *node, struct rb_root *root);
+
+#endif //C_RBTREE_H
